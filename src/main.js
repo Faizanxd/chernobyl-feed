578f60f47b1b46bb05e48caf727819af55561695
Faizan <fs8167@gmail.com>
2022-10-26 11:44:54 +0530

    Adding dark mode switcher

--- src/main.js
+++ src/main.js
@@ -1,2 +1,44 @@
 import './style.css'
 
+const moonIcon = document.getElementById("moon-icon");
+const sunIcon = document.getElementById("sun-icon");
+
+const userTheme = localStorage.getItem("theme");
+
+const systemTheme = window.matchMedia("(prefer-color-scheme: dark)").matches;
+
+const iconToggle = () =>{
+    moonIcon.classList.toggle("display-none");
+    sunIcon.classList.toggle("display-none");
+};
+
+const themeCheck =() =>{
+    if(userTheme=== "dark" || (!userTheme && systemTheme)){
+        document.documentElement.classList.add("dark");
+       moonIcon.classList.add("display-none");
+        return;
+    }
+     sunIcon.classList.add("display-none");
+};
+
+const themeSwitch =() =>{
+    if(document.documentElement.classList.contains("dark")){
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme","light");
+        iconToggle();
+        return;
+    }
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme","dark");
+    iconToggle();
+
+};
+
+moonIcon.addEventListener("click",()=>{
+    themeSwitch();
+});
+sunIcon.addEventListener("click",()=>{
+    themeSwitch();
+});
+
+themeCheck();
